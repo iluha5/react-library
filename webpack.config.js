4730162dc36fae299497c0ca40be0f344e8098f8
Ilya Rovda <i.rovda@dsec.ru>
2019-10-31 23:35:38 +0300

    feat(configs): add base tools configs

---
+++ webpack.config.js
@@ -0,0 +1,196 @@
+/* eslint-disable no-undef */
+const path = require('path');
+// const devMode = process.env.NODE_ENV !== 'production';
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'public/'),
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss'],
+        modules: ['node_modules'],
+        alias: {},
+    },
+    devtool: 'source-map',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                parallel: true,
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(sa|sc|c)ss|css$/,
+                exclude: [],
+                oneOf: [
+                    {
+                        resourceQuery: /^\?raw$/,
+                        use: [
+                            MiniCssExtractPlugin.loader,
+
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    plugins: [
+                                        require('autoprefixer')({
+                                            browsers: ['> 0%'],
+                                            cascade: false,
+                                        }),
+                                    ],
+                                },
+                            },
+                            'sass-loader',
+                        ],
+                    },
+                    {
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    importLoaders: 1,
+                                    modules: true,
+                                    localIdentName: '[local]_[hash:base64:15]',
+                                },
+                            },
+                            'sass-loader',
+                        ],
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]',
+                            context: '',
+                        },
+                    },
+                ],
+                include: [
+                    path.resolve(__dirname, 'client/fonts'),
+                ],
+            },
+            {
+                test: /config\.js/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'webfonts-loader',
+                        options: {
+                            name: '/fonts/[name].[ext]',
+                        },
+                    },
+                ],
+                include: [path.resolve(__dirname, 'client/components/Icon')],
+            },
+            {
+                // DATA IMAGES
+                test: /\.(png|jpe?g|svg)$/,
+                oneOf: [
+                    {
+                        resourceQuery: /^\?raw$/,
+                        use: [
+                            {
+                                loader: 'url-loader',
+                                options: {
+                                    limit: 8192,
+                                    name: 'icons/[name].[ext]?raw',
+                                },
+                            },
+                        ],
+                        include: [
+                            path.resolve(__dirname, 'src/icons'),
+                        ],
+                    },
+                    {
+                        use: [
+                            {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'icons/[name].[ext]?[hash]',
+                                },
+                            },
+                        ],
+                        include: [
+                            path.resolve(__dirname, 'src/icons'),
+                        ],
+                    },
+                ],
+            },
+            {
+                // IMAGES
+                test: /\.(png|jpe?g|svg|svgz)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]?[hash]',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 85,
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '80-90',
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            // webp: {
+                            //     quality: 75
+                            // }
+                        },
+                    },
+                ],
+                include: [
+                    path.resolve(__dirname, 'src/img'),
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Components',
+            meta: {
+                viewport: 'width=device-width, initial-scale=1',
+            },
+            hash: true,
+            template: 'src/indexTemplate.html',
+        }),
+    ],
+};
