91ed9c9e6996707384889188cdbc64cd576de741
Ilya Rovda <i.rovda@dsec.ru>
2019-11-10 17:06:54 +0300

    fix(app): fix buttons

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,12 +5,56 @@ import style from './style.scss';
 
 import Button from 'components/Button';
 
-function App({ buttonClick }) {
+function App({buttonClick}) {
     return (
         <div className={style['App']}>
-            <Button
-                onClick={buttonClick}
-            >Show notification</Button>
+            <h2 className={style['App-Title']}>
+                Buttons.
+            </h2>
+            <h3 className={style['App-SubTitle']}>
+                Try to click them.
+            </h3>
+            <div className={style['App-Line']}>
+                <div className={style['App-Item']}>
+                    <Button
+                        onClick={buttonClick}
+                    >
+                        Primary
+                    </Button>
+                </div>
+                <div className={style['App-Item']}>
+                    <Button
+                        onClick={buttonClick}
+                        btnType={'secondary'}
+                    >
+                        Secondary
+                    </Button>
+                </div>
+                <div className={style['App-Item']}>
+                    <Button
+                        onClick={buttonClick}
+                        btnType={'success'}
+                    >
+                        Success
+                    </Button>
+                </div>
+                <div className={style['App-Item']}>
+                    <Button
+                        onClick={buttonClick}
+                        btnType={'fake'}
+                    >
+                        Fake
+                    </Button>
+                </div>
+                <div className={style['App-Item']}>
+                    <Button
+                        onClick={buttonClick}
+                        btnType={'link'}
+                    >
+                        Link
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
