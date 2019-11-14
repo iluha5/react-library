3c255502eaeeaf2543643ceebe1b915daeb4ea8a
Ilya Rovda <i.rovda@dsec.ru>
2019-11-14 20:26:50 +0300

    fix(button): fix tests

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Button from 'components/Button';
 
-import { shallow } from 'enzyme';
-import { checkProps } from 'utils/testUtils';
+import {shallow} from 'enzyme';
+import {checkProps, findByClassName} from 'utils/testUtils';
 
 describe('Button component', () => {
     it('Should render without errors', () => {
-        const component = shallow(<Button />);
+        const component = shallow(<Button/>);
         expect(component.length).toBe(1);
     });
 
     it('Should not throw a warning', () => {
-        const expectedProps = {
+        const props = {
             className: 'TestingClass',
             btnType: 'secondary',
             size: 'm',
@@ -19,7 +19,29 @@ describe('Button component', () => {
             children: 'Test button',
             onClick: () => {},
         };
-        const propsError = checkProps(Button, expectedProps);
+        const propsError = checkProps(Button, props);
         expect(propsError).toBeUndefined();
+    });
+
+    describe('Renders', () => {
+        let wrapper;
+        let mockFunc;
+
+        beforeEach(() => {
+            mockFunc = jest.fn();
+            const props = {
+                children: 'Button text',
+                onClick: mockFunc,
+            };
+            wrapper = shallow(<Button {...props} />);
+        });
+
+        it('Should emit callback on click event', () => {
+            const button = findByClassName(wrapper, 'Btn');
+            button.simulate('click');
+
+            const callback = mockFunc.mock.calls.length;
+            expect(callback).toBe(1);
+        });
     });
 });
